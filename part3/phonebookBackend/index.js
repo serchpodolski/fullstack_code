2f507ef00d173800961a2e7af14400b22c53a0cc
Sergio Gutierrez <bropen2424@gmail.com>
2026-02-25 17:51:16 -0600

    Updatint index for port specification

--- part3/phonebookBackend/index.js
+++ part3/phonebookBackend/index.js
@@ -96,7 +96,7 @@ app.post('/api/persons', (request, response) => {
 }
 )
 
-const PORT = 3002
+const PORT = process.env.PORT || 3002
 app.listen(PORT,() => {
   console.log(`Server running on port ${PORT}`)
 })
