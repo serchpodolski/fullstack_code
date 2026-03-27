9c3af5248f49db9ba4a194d88100c3c7a74a1bd8
Sergio Gutierrez <bropen2424@gmail.com>
2026-03-27 13:14:57 -0600

    part6 complete

---
+++ part6/redux-anecdotes/src/components/Notification.jsx
@@ -0,0 +1,18 @@
+import { useSelector } from "react-redux"
+
+const Notification = () => {
+  const style = {
+    border: 'solid',
+    padding: 10,
+    borderWidth: 1,
+    marginBottom: 10
+  }
+
+  const notification = useSelector(state => state.notification)
+  // console.log(notification)
+  if(!notification) return null
+
+  return <div style={style}>{ notification }</div>
+}
+
+export default Notification
