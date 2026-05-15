2d84680909cca226d80549ef1e3eeef1fab68836
Sergio Gutierrez <82184496+serchpodolski@users.noreply.github.com>
2026-05-15 16:57:05 -0600

    Finxing the type issue in PatientService

--- part9/fs-typescript/patientor/backend/src/services/patientService.ts
+++ part9/fs-typescript/patientor/backend/src/services/patientService.ts
@@ -1,12 +1,12 @@
 import patientsData from '../../data/patients.ts';
-import type { newPatient, Patient } from '../types.ts';
+import { type NewPatient, type Patient } from '../types.ts';
 import { v1 as uuid } from 'uuid';
 
 const getPatients = (): Patient [] => {
     return patientsData;
 };
 
-const addPatient = (patient: newPatient ): Patient | undefined => {
+const addPatient = (patient: NewPatient ): Patient | undefined => {
     const id: string = uuid(); //need to cast uuid call to the correct type
     const addedPatient: Patient = {
         id,
